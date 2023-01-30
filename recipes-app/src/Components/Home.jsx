8d9a7dfd030df5be3b780076bb9053f78bb691e9
KatieEStocker <kstocker@digitalfutures.com>
2023-01-30 17:09:55 +0000

    Added a Home component

---
+++ recipes-app/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <h1>Purely Recipes</h1>
+            <p>Skipping the life stories</p>
+        </>
+    )
+}
+export default Home;
