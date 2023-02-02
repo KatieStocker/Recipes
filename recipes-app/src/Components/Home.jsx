3fe2679bb70ebaa2fee4984684bf1e9e0ccc1e1a
KatieStocker <stocker_katie@aol.co.uk>
2023-02-02 17:20:08 +0000

    amended the recipes page component to render some data from the example set

--- recipes-app/src/Components/Home.jsx
+++ recipes-app/src/Components/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import RecipeData from '../Tests/TestData/RecipeExample.json'
+//Above data will be passed as props
+
+import RecipePage from './RecipePage'
 
 const Home = () => {
     return (
         <>
-            <h1>Purely Recipes</h1>
-            <p>Skipping the life stories</p>
+            {/* ToDo: encapsulate the h1 and h3 below in a separate heading/header component */}
+            <h1>Get To The Recipes</h1>
+            <h3>Skipping the life stories</h3>
+            <span>- - - - - - - - - - - -</span>
+            <div>
+                <RecipePage data={{ RecipeData }} />
+            </div>
         </>
     )
 }
--- recipes-app/src/Components/RecipePage.jsx
+++ recipes-app/src/Components/RecipePage.jsx
@@ -1,25 +1,59 @@
 import React from 'react'
+// Below data will be used here -> select the right recipe id based on the recipe data passed through the props
+import RecipeIngredientsData from '../Tests/TestData/RecipeIngredientsExample.json'
+import RecipeInstructionsData from '../Tests/TestData/RecipeInstructionsExample.json'
 
-const RecipePage = () => {
+const RecipePage = ({ data }) => {
+
+    let recipeIdValue = data.RecipeData[0].id
+
+    // ToDo: make this it's own separate component (same goes for instruction data)
+    // Note: for the unit, can use an enum, or similar, to render the initialised measurement OR store the unit as that value
+    const renderIngredients = () => {
+        return RecipeIngredientsData.map((i) => (
+            <>
+                {i.recipeId === recipeIdValue ? <p>{i.quantity} {i.unit} {i.ingredient}</p> : <p>Test</p>}
+            </>
+        ))
+    }
+
+    const renderInstructions = () => {
+        return RecipeInstructionsData.map((i) => (
+            <>
+                {i.recipeId === recipeIdValue ? <p>{i.step}. {i.instruction}</p> : <p>Test</p>}
+            </>
+        ))
+    }
+
+    const renderRecipe = () => {
+        const recipeData = data.RecipeData[0];
+
+        return (
+            <div>
+                <h1>{recipeData.title}</h1>
+                <img src={recipeData.image_url} alt="recipe-url" />
+                <h5>{recipeData.description}</h5>
+                <p>- - - - - - - - - - - - - - - - - - - - - - - - -</p>
+                <div>
+                    {/* ToDo - add some logic to render hours and minutes for both times below */}
+                </div>
+                <p>- - - - - - - - - - - - - - - - - - - - - - - - -</p>
+            </div>
+        )
+    }
+
     return (
         <>
-            <h1>Example Recipe Layout</h1>
-            <h2>Title of Recipe</h2>
-            <p>-------------------</p>
             <div>
-                <div className="image-div">
-                    <img></img>
-                </div>
-                <div className="ingredient-list-div">
-                    <p>400g Plain White Flour</p>
-                </div>
-                <div className="other-info-div">
-                    <p>Prep time: 10 mins</p>
-                    <p>Cook time: 20 mins</p>
-                </div>
-                <div className="instructions-div">
-                    <p>Step 1: Pre-heat the oven to...</p>
-                </div>
+                {renderRecipe()}
+            </div>
+            <div>
+                <h3>Ingredients</h3>
+                {renderIngredients()}
+            </div>
+            <div>
+                <h3>Instructions</h3>
+                {renderInstructions()}
             </div>
         </>
     )
