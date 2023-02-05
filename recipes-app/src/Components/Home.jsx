109aa0844987e5112022a53ea69f85a8e5d3d023
KatieStocker <stocker_katie@aol.co.uk>
2023-02-05 16:45:21 +0000

    amended title and caption

--- recipes-app/src/Components/Home.jsx
+++ recipes-app/src/Components/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import RecipeData from '../Tests/TestData/RecipeExample.json'
-//Above data will be passed as props
 
 import RecipePage from './Recipe/RecipePage'
 
@@ -8,8 +7,8 @@ const Home = () => {
     return (
         <>
             {/* ToDo: encapsulate the h1 and h3 below in a separate heading/header component */}
-            <h1>Get To The Recipes</h1>
-            <h3>Skipping the life stories</h3>
+            <h1>Straight To The Recipe</h1>
+            <h3>Skipping the life stories and getting straight to the point</h3>
             <span>- - - - - - - - - - - -</span>
             <div>
                 <RecipePage data={{ RecipeData }} />
