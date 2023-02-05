88616b94028ec44abca3bdd3770c1f4036fc6ab0
KatieStocker <stocker_katie@aol.co.uk>
2023-02-05 20:53:04 +0000

    added a Title component and restructured the Home component

--- recipes-app/src/Components/Home.jsx
+++ recipes-app/src/Components/Home.jsx
@@ -2,17 +2,14 @@ import React from 'react'
 import RecipeData from '../Tests/TestData/RecipeExample.json'
 
 import RecipePage from './Recipe/RecipePage'
+import TitleSection from './TitleSection'
 
 const Home = () => {
     return (
         <>
-            {/* ToDo: encapsulate the h1 and h3 below in a separate heading/header component */}
-            <h1>Straight To The Recipe</h1>
-            <h3>Skipping the life stories and getting straight to the point</h3>
+            <TitleSection />
             <span>- - - - - - - - - - - -</span>
-            <div>
-                <RecipePage data={{ RecipeData }} />
-            </div>
+            <RecipePage data={{ RecipeData }} />
         </>
     )
 }
---
+++ recipes-app/src/Components/TitleSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TitleSection = () => {
+    return (
+        <>
+            <h1>Straight To The Recipe</h1>
+            <h3>Skipping the life stories and getting straight to the point</h3>
+        </>
+    )
+}
+export default TitleSection;
