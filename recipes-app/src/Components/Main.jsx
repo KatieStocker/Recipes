cd18f1b1482723b0d5951e6ef43ef5553e2de6b2
KatieStocker <stocker_katie@aol.co.uk>
2023-02-15 15:33:33 +0000

    alphabetised route paths

--- recipes-app/src/Components/Main.jsx
+++ recipes-app/src/Components/Main.jsx
@@ -34,11 +34,11 @@ const Main = () => {
                 <div className={open ? "main" : "main-collapsed-side-bar"}>
                     <Routes>
                         <Route exact path="/" element={<HomePage data={{ RecipeData }} className="sub-page" />}></Route>
-                        <Route exact path="/recipe/:id" element={<RecipePage data={{ selectedRecipe }} className="sub-page" />}></Route>
-                        <Route path="/all-recipes" element={<FilteredRecipes data={{ RecipeData, filter: "" }} className="sub-page" />}></Route>
-                        <Route path="/food" element={<FilteredRecipes data={{ RecipeData, filter: "Food" }} />}></Route>
-                        <Route path="/drinks" element={<FilteredRecipes data={{ RecipeData, filter: "Drink" }} />}></Route>
                         <Route path="/about" element={<About />}></Route>
+                        <Route path="/all-recipes" element={<FilteredRecipes data={{ RecipeData, filter: "" }} className="sub-page" />}></Route>
+                        <Route path="/drinks" element={<FilteredRecipes data={{ RecipeData, filter: "Drink" }} />}></Route>
+                        <Route path="/food" element={<FilteredRecipes data={{ RecipeData, filter: "Food" }} />}></Route>
+                        <Route exact path="/recipe/:id" element={<RecipePage data={{ selectedRecipe }} className="sub-page" />}></Route>
                     </Routes>
                 </div>
             </Router>
