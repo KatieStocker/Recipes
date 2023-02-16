6dee36d5847d247ace8674fb659a82e6b2c2f987
KatieStocker <stocker_katie@aol.co.uk>
2023-02-16 10:56:13 +0000

    added styling

--- recipes-app/src/Components/Home/Featured.jsx
+++ recipes-app/src/Components/Home/Featured.jsx
@@ -7,7 +7,7 @@ const Featured = ({ data }) => {
 
     return (
         <div className="mt-3">
-            <h2>Featured Recipes</h2>
+            <h2 className="mb-4 mt-4">Featured Recipes</h2>
             <Recipes data={{ RecipeData, filter: "Drink" }} />
         </div>
     )
