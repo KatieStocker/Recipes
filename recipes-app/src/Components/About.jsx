ae161bc608483bbc76a2c64edefd8b52644a26f7
KatieStocker <stocker_katie@aol.co.uk>
2023-02-16 10:36:09 +0000

    updated the About Us section

--- recipes-app/src/Components/About.jsx
+++ recipes-app/src/Components/About.jsx
@@ -3,7 +3,11 @@ const About = () => {
 
     return (
         <>
-            <h1>About Us</h1>
+            <h1 className="about-title">About Us</h1>
+            <div className="about-text">
+                <p>Straight to the Recipe was created as a project based on a problem I, and I am sure many other recipe followers, encounter when you just want to find a recipe and not have to scroll through paragraphs and paragraphs of text about the creators life-story.</p>
+                <p>This website aims to provide a better presentation of recipes to users, skipping the life-story entirely and going straight to the point by keeping each section simple to find and easy to browse through.</p>
+            </div>
         </>
     )
 }
