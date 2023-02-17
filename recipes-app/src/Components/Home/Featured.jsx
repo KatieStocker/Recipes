4bb5e283755879cd91b46b97d005fea29389af99
KatieStocker <stocker_katie@aol.co.uk>
2023-02-17 12:58:41 +0000

    added temporary function to randomly select a filter value and change the featured recipes displayed

--- recipes-app/src/Components/Home/Featured.jsx
+++ recipes-app/src/Components/Home/Featured.jsx
@@ -4,11 +4,21 @@ const Featured = ({ data }) => {
 
     const { RecipeData } = data;
 
+    let filterString = ""
+
+    // temporary function to change the recipes shown in the Featured section 
+    const randomFilter = () => {
+        const filterValues = ["", "Food", "Drink"]
+        const index = Math.floor(Math.random() * 3)
+        filterString = filterValues[index]
+    }
+    randomFilter()
+    //
 
     return (
         <div className="mt-3">
             <h2 className="mb-4 mt-4">Featured Recipes</h2>
-            <Recipes data={{ RecipeData, filter: "Drink" }} />
+            <Recipes data={{ RecipeData, filter: filterString }} />
         </div>
     )
 }
