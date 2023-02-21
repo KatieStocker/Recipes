cb892e780bb7a5f1b762575e6ddafc3e1f58cb77
KatieStocker <stocker_katie@aol.co.uk>
2023-02-21 15:17:31 +0000

    made changes to the seasonal data - now called Categories. Seasonal Page added which will render those categories which have isSeason === true

--- recipes-app/src/Components/Seasonal/SeasonalPage.jsx
+++ recipes-app/src/Components/Seasonal/SeasonalPage.jsx
@@ -1,4 +1,5 @@
 import { renderRecipes } from "../../utils/RecipesUtils";
+import { categoriesData } from '../../lib/categoriesData'
 
 const SeasonalPage = ({ data }) => {
     const { RecipeData } = data;
@@ -8,13 +9,33 @@ const SeasonalPage = ({ data }) => {
         return renderRecipes(recipes)
     }
 
+    const filterCategories = () => {
+        return categoriesData.filter((i) => { return i.isSeason })
+    }
+
+    const renderCategories = () => {
+        let categories = filterCategories().sort((a, b) => a.name.localeCompare(b.name))
+
+        return categories.map((i) => (
+            <div key={i.id} className="col-3">
+                {/* ToDo: add an onclick function to each of these which will change the rendered recipe thumbnails based on the option clicked */}
+                <p key={i.id}>{i.name}</p>
+            </div>
+        ))
+    }
+
     return (
         <div>
             <h1 className="mb-3">Seasonal Recipes</h1>
+            <h3 className="mb-3">Categories</h3>
             <div className="row">
+                {renderCategories()}
+            </div>
+            {/* Dynamically change what is rendered below based on the selected category from the above renderCategories */}
+            <div className="row mt-5">
                 {renderSeasonalRecipes()}
             </div>
-        </div>
+        </div >
     )
 }
 export default SeasonalPage;
---
+++ recipes-app/src/lib/categoriesData.js
@@ -0,0 +1,246 @@
+// ToDo: Use this data set for clickable category option buttons - the other CategoryOptionsExample.json dataset will be used within select drop-down options in the edit/update pages for recipes.
+export const categoriesData = [
+    {
+        id: 1,
+        name: "New Year",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [1, 2, 17, 28],
+        icon: ""
+    },
+    {
+        id: 2,
+        name: "Pancake Day",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [3],
+        icon: ""
+    },
+    {
+        id: 3,
+        name: "Valentine's Day",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [4],
+        icon: ""
+    },
+    {
+        id: 4,
+        name: "St. David's Day",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [5],
+        icon: ""
+    },
+    {
+        id: 5,
+        name: "St. Patrick's Day",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [6],
+        icon: ""
+    },
+    {
+        id: 6,
+        name: "Mother's and Father's Day",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [7, 9],
+        icon: ""
+    },
+    {
+        id: 7,
+        name: "Easter",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [8],
+        icon: ""
+    },
+    {
+        id: 8,
+        name: "Halloween",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [10],
+        icon: ""
+    },
+    {
+        id: 9,
+        name: "Diwali",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [11],
+        icon: ""
+    },
+    {
+        id: 10,
+        name: "Bonfire Night",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [12],
+        icon: ""
+    },
+    {
+        id: 11,
+        name: "Hanukkah",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [13],
+        icon: ""
+    },
+    {
+        id: 12,
+        name: "Christmas",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [14, 15, 16],
+        icon: ""
+    },
+    {
+        id: 13,
+        name: "Ramadan",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [22],
+        icon: ""
+    },
+    {
+        id: 14,
+        name: "Eid",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [23],
+        icon: ""
+    },
+    {
+        id: 15,
+        name: "Thanksgiving",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [24],
+        icon: ""
+    },
+    {
+        id: 16,
+        name: "Burns Night",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [25],
+        icon: ""
+    },
+    {
+        id: 17,
+        name: "Birthday",
+        isSeason: false,
+        isOccasion: true,
+        isDietOption: false,
+        optionIds: [27],
+        icon: ""
+    },
+    {
+        id: 18,
+        name: "Anniversary",
+        isSeason: false,
+        isOccasion: true,
+        isDietOption: false,
+        optionIds: [4, 26],
+        icon: ""
+    },
+    {
+        id: 19,
+        name: "Wedding",
+        isSeason: false,
+        isOccasion: true,
+        isDietOption: false,
+        optionIds: [28, 29],
+        icon: ""
+    },
+    {
+        id: 20,
+        name: "Dinner Party",
+        isSeason: false,
+        isOccasion: true,
+        isDietOption: false,
+        optionIds: [28],
+        icon: ""
+    },
+    {
+        id: 21,
+        name: "Vegetarian",
+        isSeason: false,
+        isOccasion: false,
+        isDietOption: true,
+        optionIds: [30],
+        icon: ""
+    },
+    {
+        id: 22,
+        name: "Vegan",
+        isSeason: false,
+        isOccasion: false,
+        isDietOption: true,
+        optionIds: [31],
+        icon: ""
+    },
+    {
+        id: 23,
+        name: "Free From",
+        isSeason: false,
+        isOccasion: false,
+        isDietOption: true,
+        optionIds: [32, 33, 34, 35],
+        icon: ""
+    },
+    {
+        id: 24,
+        name: "Winter Collection",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [1, 2, 3, 4, 13, 14, 15, 16, 17, 18, 25],
+        icon: ""
+    },
+    {
+        id: 25,
+        name: "Spring Collection",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [5, 6, 7, 8, 19],
+        icon: ""
+    },
+    {
+        id: 26,
+        name: "Summer Collection",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [9, 20],
+        icon: ""
+    },
+    {
+        id: 27,
+        name: "Autumn Collection",
+        isSeason: true,
+        isOccasion: false,
+        isDietOption: false,
+        optionIds: [10, 11, 12, 21, 24],
+        icon: ""
+    }
+]
