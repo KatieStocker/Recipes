9292954e47af6d5a9267a88e1ef97aa156df7e48
KatieStocker <stocker_katie@aol.co.uk>
2023-02-27 15:41:42 +0000

    removed unused library function

--- recipes-app/src/Tests/RecipeTests/Recipes.test.js
+++ recipes-app/src/Tests/RecipeTests/Recipes.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Recipes from '../../Components/Recipe/Recipes';
 import { testMultipleRecipes } from '../TestData/sampleRecipes';
