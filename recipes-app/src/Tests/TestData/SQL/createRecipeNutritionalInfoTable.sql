ae1944de978c0c3ba27a8fea70f29aa32b988965
KatieStocker <stocker_katie@aol.co.uk>
2023-03-13 14:20:19 +0000

    corrected SQL query

--- recipes-app/src/Tests/TestData/SQL/createRecipeNutritionalInfoTable.sql
+++ recipes-app/src/Tests/TestData/SQL/createRecipeNutritionalInfoTable.sql
@@ -12,16 +12,16 @@ CREATE TABLE recipe_nutritional_info (
     salt NUMERIC(10, 3) NOT NULL DEFAULT 0
 );
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, saturates, carbs, sugar, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Easy Chocolate Cake";), 14, 523, 29, 18, 59, 47, 2, 6, 0.6);
+VALUES((SELECT id FROM recipe WHERE title = 'Easy Chocolate Cake'), 14, 523, 29, 18, 59, 47, 2, 6, 0.6);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, saturates, carbs, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Leek and Potato Soup";), 6, 252, 18, 11, 18, 3, 5, 0.7);
+VALUES((SELECT id FROM recipe WHERE title = 'Leek and Potato Soup'), 6, 252, 18, 11, 18, 3, 5, 0.7);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, carbs, sugar, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Passion Fruit Martini";), 2, 224, 16, 16, 2, 1, 0.03);
+VALUES((SELECT id FROM recipe WHERE title = 'Passion Fruit Martini'), 2, 224, 16, 16, 2, 1, 0.03);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, saturates, carbs, sugar, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Easy Pancakes";), 12, 61, 2, 1, 7, 1, 3, 0.1);
+VALUES((SELECT id FROM recipe WHERE title = 'Easy Pancakes'), 12, 61, 2, 1, 7, 1, 3, 0.1);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, carbs, sugar, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Classic Mulled Wine";), 6, 196, 0.1, 13.8, 13.6, 0.2, 0.3, 0.03);
+VALUES((SELECT id FROM recipe WHERE title = 'Classic Mulled Wine'), 6, 196, 0.1, 13.8, 13.6, 0.2, 0.3, 0.03);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, saturates, carbs, sugar, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Simnel Cake";), 12, 619, 29, 12, 77, 60, 2, 10, 0.65);
+VALUES((SELECT id FROM recipe WHERE title = 'Simnel Cake'), 12, 619, 29, 12, 77, 60, 2, 10, 0.65);
 INSERT INTO recipe_nutritional_info (recipe_id, servings, calories, fat, saturates, carbs, sugar, fibre, protein, salt)
-VALUES((SELECT id FROM recipe WHERE title = "Bread Puffs";), 6, 320, 15, 2, 40, 2, 2, 7, 0.07);
+VALUES((SELECT id FROM recipe WHERE title = 'Bread Puffs'), 6, 320, 15, 2, 40, 2, 2, 7, 0.07);
