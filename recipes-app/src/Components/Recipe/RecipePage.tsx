84deab7ada0661dc408fe5482c909d0722fa5f9b
KatieStocker <stocker_katie@aol.co.uk>
2023-04-03 15:03:05 +0100

    added TS version of RecipePage component

---
+++ recipes-app/src/Components/Recipe/RecipePage.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import '../../CSS/RecipePage.css';
+
+import BackButton from '../Navigation/BackButton.tsx';
+import RecipePageIngredients from './RecipePageIngredients.tsx';
+import RecipePageInstructions from './RecipePageInstructions.tsx';
+import RecipePageRecipe from './RecipePageRecipe.tsx';
+
+import ingredients from '../../Tests/TestData/RecipeIngredientsExample.json';
+import instructions from '../../Tests/TestData/RecipeInstructionsExample.json';
+
+interface RecipePageProps {
+    data: {
+        selectedRecipe: {
+            id: number
+        }
+    },
+    className?: string
+}
+
+const RecipePage: React.FC<RecipePageProps> = ({ data }) => {
+
+    const { state } = useLocation();
+    const { selectedRecipe } = data;
+    let recipeData = selectedRecipe;
+
+    if (state !== null && state.length !== 0) {
+        const { recipe } = state;
+        recipeData = recipe;
+    }
+
+    return (
+        <div className="recipe-page">
+            <BackButton />
+            <div>
+                <RecipePageRecipe data={{ recipeData }} />
+                <RecipePageIngredients data={{ ingredients, recipeId: recipeData.id }} />
+                <RecipePageInstructions data={{ instructions, recipeId: recipeData.id }} />
+            </div>
+        </div>
+    )
+}
+export default RecipePage;
