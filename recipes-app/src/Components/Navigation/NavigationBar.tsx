c18cccee08c0f9053358ebefa90e764753a44a7e
KatieStocker <stocker_katie@aol.co.uk>
2023-04-03 14:47:49 +0100

    added TypeScript version of NavigationBar component

---
+++ recipes-app/src/Components/Navigation/NavigationBar.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { navData } from '../../lib/navData';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import '../../CSS/NavBar.css'
+
+interface NavigationBarProps {
+    data: {
+        open: boolean,
+        toggleOpen: (() => void)
+    }
+}
+
+const NavigationBar: React.FC<NavigationBarProps> = ({ data }) => {
+
+    const { open, toggleOpen } = data;
+
+    return (
+        <div className={open ? "side-nav" : "side-navClosed"}>
+            <div className="menuBtnDiv" onClick={toggleOpen}>
+                <button className="menuBtn">
+                    {open ? <KeyboardDoubleArrowLeftIcon className="arrowIcon" /> : <KeyboardDoubleArrowRightIcon className="arrowIcon collapsed" />}
+                </button>
+            </div>
+            {navData.map(item => {
+                return <Link key={item.id} className={open ? "side-item" : "side-itemClosed"} to={item.link}>
+                    {item.icon}
+                    <span className={open ? "linkText" : "linkTextClosed"}>{item.text}</span>
+                </Link>
+            })}
+        </div>
+    )
+}
+export default NavigationBar;
