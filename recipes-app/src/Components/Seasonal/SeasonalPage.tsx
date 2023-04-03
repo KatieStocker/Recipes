337cbcb5dbc21d0b95f1e2971d16e24b271e637b
KatieStocker <stocker_katie@aol.co.uk>
2023-04-03 17:39:57 +0100

    added a TS version of the SeasonalPage component

---
+++ recipes-app/src/Components/Seasonal/SeasonalPage.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import { useState } from 'react'
+import '../../CSS/SeasonalPage.css'
+
+import { renderRecipes } from "../../utils/RecipesUtils.js";
+import { categoriesData } from '../../lib/categoriesData.js';
+import { Recipe } from '../../Types/Recipe.ts';
+import { CategoriesData } from '../../Types/CategoriesData.ts';
+
+interface SeasonalPageProps {
+    data: {
+        RecipeData: Recipe[]
+    }
+}
+
+const SeasonalPage: React.FC<SeasonalPageProps> = ({ data }) => {
+    const { RecipeData } = data;
+    const seasonalData = RecipeData.filter((i) => { return i.isSeasonal })
+
+    const [recipes, setRecipes] = useState(seasonalData)
+    const [selectedCategory, setSelectedCategory] = useState("")
+
+    const getFilteredCategories = () => {
+        return categoriesData.filter((i) => { return i.isSeason })
+    }
+
+    const getFilteredRecipes = (id: number) => {
+        return seasonalData.filter((i) => { return i.categoryIds.includes(id) })
+    }
+
+    const handleOptionClick = (selectedCategory: CategoriesData) => {
+        setSelectedCategory(selectedCategory.name)
+        updateRecipes(selectedCategory.optionIds)
+    }
+
+    const updateRecipes = (categoryOptionIds: number[]) => {
+        let filteredRecipeArray: Recipe[] = []
+        for (let i = 0; i < categoryOptionIds.length; i++) {
+            const filteredRecipe = getFilteredRecipes(categoryOptionIds[i])
+            if (filteredRecipe.length > 0 && !filteredRecipeArray.includes(filteredRecipe[0])) {
+                filteredRecipeArray.push(filteredRecipe[0])
+            }
+        }
+        setRecipes(filteredRecipeArray)
+    }
+
+    const renderCategories = () => {
+        let categories = getFilteredCategories().sort((a, b) => a.name.localeCompare(b.name))
+
+        return categories.map((i) => (
+            <div key={i.id} className={`col-3 ${selectedCategory !== i.name ? "individual-category" : "individual-category-selected"}`}>
+                <p key={i.id} className="category-option" onClick={() => handleOptionClick(i)}>{i.name}</p>
+            </div>
+        ))
+    }
+
+    const renderNoneAvailableMessage = () => {
+        return (
+            <div className="no-recipes">
+                <p>No Recipes Yet Available for the Category: {selectedCategory}</p>
+            </div>
+        )
+    }
+
+    const renderRecipesSection = () => {
+        return (
+            <div>
+                <h3>{selectedCategory} Recipes</h3>
+                <div className="row mt-5">
+                    {renderRecipes(recipes)}
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <h1 className="mb-3">Seasonal Recipes</h1>
+            <h3 className="mb-3">Categories</h3>
+            <div className="row categories">
+                {renderCategories()}
+            </div>
+            <div className="mt-4">
+                {recipes.length > 0 ? renderRecipesSection() : renderNoneAvailableMessage()}
+            </div>
+        </div >
+    )
+}
+export default SeasonalPage;
