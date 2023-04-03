aad66355d38d7fbf9547ec87bcf2a087479b060b
KatieStocker <stocker_katie@aol.co.uk>
2023-04-03 14:48:41 +0100

    added TS version of About component

---
+++ recipes-app/src/Components/About.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About: React.FC = () => {
+
+    return (
+        <>
+            <h1 className="mb-3">About Us</h1>
+            <div className="about-text">
+                <p>Straight to the Recipe was created as a project based on a problem I, and I am sure many other recipe readers, encounter when you just want to find a recipe and not have to scroll through paragraphs and paragraphs of text about the creators life-story.</p>
+                <p>This website aims to provide a better presentation of recipes to users, skipping the life-story entirely and going straight to the point by keeping each section simple to find and easy to browse through.</p>
+
+                <h3 className="mb-3 mt-5">About me</h3>
+                <div>
+                    <p>A software developer from the North of England looking for ways to improve upon various features in day-to-day use websites or applications.</p>
+                    <p>Currently positioned in a Cloud-based DevOps and Innovation role, I have developed skills and knowledge in many areas of the tech world, and I will continue to learn and improve these skills as much as I can with projects such as this one.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+export default About;
