c68bef45442d5bd6b3490b7c34a6d3be29d685e7
KatieStocker <stocker_katie@aol.co.uk>
2023-04-24 16:43:43 +0100

    added query to get recipes with inner joins to instruction and ingredient

--- backend/src/db/queries/recipe.js
+++ backend/src/db/queries/recipe.js
@@ -20,7 +20,18 @@ const getRecipeById = (req, res) => {
     })
 }
 
+const getRecipeForRecipePage = (req, res) => {
+    const id = parseInt(req.params.id)
+    db.query(`${query.selectAllFrom} recipe ${query.innerJoin} recipe_ingredient ${query.on} recipe.id = recipe_ingredient.recipe_id ${query.innerJoin} recipe_instruction ${query.on} recipe.id = recipe_instruction.recipe_id ${query.where} recipe.id = $1`, [id], (err, results) => {
+        if (err) {
+            throw err
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
 module.exports = {
     getRecipes,
-    getRecipeById
+    getRecipeById,
+    getRecipeForRecipePage
 }
