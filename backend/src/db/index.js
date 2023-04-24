45028424c1d2a680d044ed2cc4e71940cb2f49ca
KatieStocker <stocker_katie@aol.co.uk>
2023-04-24 14:37:47 +0100

    updated to use a separate file to create the pool, and another for exporting all the queries in one place

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,31 +1,6 @@
-require('dotenv').config({ path: __dirname + '../../.env.local' });
-const {
-    DB_USER,
-    DB_HOST,
-    DB,
-    DB_PASS,
-    DB_PORT,
-} = process.env
-
-const Pool = require('pg').Pool;
-const pool = new Pool({
-    user: DB_USER,
-    host: DB_HOST,
-    database: DB,
-    password: DB_PASS,
-    port: DB_PORT,
-});
-
-const recipeQueries = require('./queries/recipe');
-
-const getRecipes = (req, res) => {
-    recipeQueries.getRecipes(req, res, pool)
-};
-const getRecipeById = (req, res) => {
-    recipeQueries.getRecipeById(req, res, pool)
-};
+const recipe = require('./queries/recipe')
 
 module.exports = {
-    getRecipes,
-    getRecipeById
+    getRecipes: recipe.getRecipes,
+    getRecipeById: recipe.getRecipeById,
 }
