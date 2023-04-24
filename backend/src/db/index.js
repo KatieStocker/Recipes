100d1a993b82d9714ec8890657bc29938b19a224
KatieStocker <stocker_katie@aol.co.uk>
2023-04-24 16:20:01 +0100

    added exports for ingredient and instruction queries

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,6 +1,16 @@
 const recipe = require('./queries/recipe')
+const ingredient = require('./queries/recipeIngredient')
+const instruction = require('./queries/recipeInstruction')
 
 module.exports = {
     getRecipes: recipe.getRecipes,
     getRecipeById: recipe.getRecipeById,
+
+    getIngredient: ingredient.getIngredients,
+    getIngredientById: ingredient.getIngredientById,
+    getIngredientByRecipeId: ingredient.getIngredientByRecipeId,
+
+    getInstruction: instruction.getInstructions,
+    getInstructionById: instruction.getInstructionById,
+    getInstructionByRecipeId: instruction.getInstructionByRecipeId,
 }
