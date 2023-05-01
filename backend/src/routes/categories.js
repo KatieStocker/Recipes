f88066f79c43a77319a91dfdafca85887181fdf0
KatieStocker <stocker_katie@aol.co.uk>
2023-05-01 21:33:45 +0100

    added categoryGroup routes

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -4,5 +4,7 @@ const db = require('../db/index');
 
 router.get('/', db.getCategories)
 router.get('/:id', db.getCategoryById);
+router.get('/groups', db.getCategoryGroups);
+router.get('/groups/:id', db.getCategoryGroupById);
 
 module.exports = router;
