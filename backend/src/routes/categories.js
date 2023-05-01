fc75aaa42272d226aefedd774306cfb0439c194d
KatieStocker <stocker_katie@aol.co.uk>
2023-05-01 21:38:57 +0100

    added routes for category option

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -6,5 +6,7 @@ router.get('/', db.getCategories)
 router.get('/:id', db.getCategoryById);
 router.get('/groups', db.getCategoryGroups);
 router.get('/groups/:id', db.getCategoryGroupById);
+router.get('/options', db.getCategoryOptions);
+router.get('/options/:id', db.getCategoryOptionById);
 
 module.exports = router;
