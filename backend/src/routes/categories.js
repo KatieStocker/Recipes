11c524a913c595a33323d979db4486a1498f75d8
KatieStocker <stocker_katie@aol.co.uk>
2023-05-04 16:20:56 +0100

    added routes

--- backend/src/routes/categories.js
+++ backend/src/routes/categories.js
@@ -8,5 +8,9 @@ router.get('/groups', db.getCategoryGroups);
 router.get('/groups/:id', db.getCategoryGroupById);
 router.get('/options', db.getCategoryOptions);
 router.get('/options/:id', db.getCategoryOptionById);
+router.get('/group_options/', db.getCategoryGroupOptions);
+router.get('/group_options/:id', db.getCategoryGroupOptionById);
+router.get('/group_options/group/:id', db.getCategoryGroupOptionByGroupId);
+router.get('/group_options/option/:id', db.getCategoryGroupOptionByOptionId);
 
 module.exports = router;
